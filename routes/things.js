2332624f2cda6ec9432b0916fcde0e3d89d8fccd
Alan Henry <alandavidhenry@gmail.com>
2022-08-26 21:52:38 +0100

    Add error handling

--- routes/things.js
+++ routes/things.js
@@ -1,53 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const Thing = require('../models/things');
+const catchAsync = require('../utils/catchAsync');
+const expressError = require('../utils/expressError');
+
+/* Error routes */
+router.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
+router.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+  res.status(statusCode).render('error', { err });
+});
 
 /* GET Render things index page */
 router.get('/index', async (req, res, next) => {
 	const things = await Thing.find({});
 	res.render('things/index', { things });
-  });
+});
   
-  /* GET Render add a new thing page */
-  router.get('/new', (req, res, next) => {
+/* GET Render add a new thing page */
+router.get('/new', (req, res, next) => {
 	res.render('things/new');
-  });
+});
   
-  /* POST Add a new thing to the database and redirect to home */
-  router.post('/', validateThing, async (req, res, next) => {
+/* POST Add a new thing to the database and redirect to home */
+router.post('/', validateThing, catchAsync(async (req, res, next) => {
 	const thing = new Thing(req.body.thing);
 	await thing.save();
 	res.redirect('/');
-  });
+}));
   
-  /* GET Render thing using id page */
-  router.get('/:id', async (req, res, next) => { 
-  /* (CANNOT USE PARAMS IN GET) */
+/* GET Render thing using id page */
+router.get('/:id', catchAsync(async (req, res, next) => { 
 	const thing = await Thing.findById(req.params.id);
 	res.render('things/show', { thing });
-  });
+}));
   
-  /* GET Render thing edit page (if no things exist, redirect to home */
-  router.get('/:id/edit', async (req, res, next) => {
+/* GET Render thing edit page (if no things exist, redirect to home */
+  router.get('/:id/edit', catchAsync(async (req, res, next) => {
 	const thing = await Thing.findById(req.params.id);
 	if(!thing) {
 	  res.redirect('/');
 	}
 	res.render('things/edit', { thing });
-  });
+}));
   
-  /* PUT submit thing edit to database and redirect to the index page */
-  router.put('/:id/edit', validateThing, async (req, res, next) => {
+/* PUT submit thing edit to database and redirect to the index page */
+  router.put('/:id/edit', validateThing, catchAsync(async (req, res, next) => {
 	const { id } = req.params;
 	const thing = await Thing.findByIdAndUpdate(id, { ...req.body.thing });
 	res.redirect('/things/index')
-  });
+}));
   
-  /* DELETE Delete thing and redirect to the index page */
-  router.delete('/:id', async (req, res, next) => {
+/* DELETE Delete thing and redirect to the index page */
+  router.delete('/:id', catchAsync(async (req, res, next) => {
 	const { id } = req.params;
 	await Thing.findByIdAndDelete(id);
 	res.redirect('/things/index');
-  });
+}));
 
 module.exports = router;
